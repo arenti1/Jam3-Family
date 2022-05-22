f07a99792dc3bd0b0567dc6709d2a4a5b3e76518
Arent Dollapaj <arent.dollapaj@epitech.eu>
2022-05-22 14:26:20 +0200

    first commit

---
+++ include/my.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2022
+** Untitled (Workspace)
+** File description:
+** my
+*/
+
+#ifndef MY_HPP_
+#define MY_HPP_
+
+class Main
+{
+    public:
+        short state = 0;
+};
+
+#endif /* !MY_HPP_ */
---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2022
+** Untitled (Workspace)
+** File description:
+** main
+*/
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include "../include/my.hpp"
+
+int main(int argc, char **argv)
+{
+    Main m;
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Learn Piano");
+
+    while (window.isOpen()){
+        if (m.state == 0){
+            start_game();
+        }
+        else if (m.state == 1){
+            quiz1();
+        }
+        else if (m.state == 2){
+            quiz1();
+        }
+        else if (m.state == 3){
+            end_game();
+        }
+        window.display();
+    }
+    return 0; 
+}
